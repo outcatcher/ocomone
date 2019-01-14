35c03d7015b990d311fd772633ae191b35bb8094
Anton Kachurin <katchuring@gmail.com>
2019-01-14 14:07:58 +0300

    Refactored browser.py

--- ocomone-selene/ocomone_selene/browser.py
+++ ocomone-selene/ocomone_selene/browser.py
@@ -5,34 +5,33 @@ from selene.browsers import BrowserName
 from selenium import webdriver
 
 
+def _headless_browser(browser_name: BrowserName):
+    if browser_name == BrowserName.CHROME:
+        from webdriver_manager.chrome import ChromeDriverManager as Manager
+        from selenium.webdriver.chrome.options import Options
+        drv_cls = webdriver.Chrome
+    elif browser_name == BrowserName.FIREFOX:
+        from webdriver_manager.firefox import GeckoDriverManager as Manager
+        from selenium.webdriver.firefox.options import Options
+        drv_cls = webdriver.Firefox
+    else:
+        raise AttributeError(f"Invalid browser: {browser_name}")
+    options = Options()
+    driver = drv_cls(executable_path=Manager().install(), options=options)
+    browser.set_driver(driver)
+
+
 def headless_chrome():
     """Configure system to use headless chrome"""
-    from selenium.webdriver.chrome.options import Options as ChromeOptions
-    from webdriver_manager.chrome import ChromeDriverManager
-
-    options = ChromeOptions()
-    options.headless = True
-    options.add_argument('--headless')
-    driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), chrome_options=options)
-    browser.set_driver(driver)
+    _headless_browser(BrowserName.CHROME)
 
 
 def headless_firefox():
     """Configure system to use headless firefox"""
-    from selenium.webdriver.firefox.options import Options as FirefoxOptions
-    from webdriver_manager.firefox import GeckoDriverManager
-
-    options = FirefoxOptions()
-    options.headless = True
-    browser.driver()
-    driver = webdriver.Firefox(executable_path=GeckoDriverManager().install(), options=options)
-    browser.set_driver(driver)
+    _headless_browser(BrowserName.FIREFOX)
 
 
 def headless_default():
     """Configure default browser to be headless"""
     browser_name = config.browser_name
-    if browser_name == BrowserName.CHROME:
-        headless_chrome()
-    elif browser_name == BrowserName.FIREFOX:
-        headless_firefox()
+    _headless_browser(browser_name)
