ddc8ff4e60df926b6cdd8b9dab1728f7552d7f98
Anton Kachurin <katchuring@gmail.com>
2019-01-14 10:33:35 +0300

    Adde ``Wired`` to package imports

--- ocomone-selene/ocomone_selene/__init__.py
+++ ocomone-selene/ocomone_selene/__init__.py
@@ -1,4 +1,4 @@
 """Selene tweaks and features"""
 
 from .bys import by_id, by_label
-from .wiring import Wireable, register_setter, register_strategy, register_without_setter, wired
+from .wiring import Wireable, Wired, register_setter, register_strategy, register_without_setter, wired
